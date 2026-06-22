cc628f415a0f2434713e4c8a4833590a3e7a4cb5
Haloch1 <adnan.sadikhli1@gmail.com>
2026-06-22 15:41:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -885,7 +885,7 @@ app.get("/api/account", async (req, res) => {
   } catch (error) {
     res.status(error.status || 500).json({
       error:
-        error instanceof Error ? error.message : "Unable to load account data right now.",
+        error instanceof Error ? error.message : "",
     });
   }
 });
